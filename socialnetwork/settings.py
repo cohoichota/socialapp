a2d590277f4bfa0ba524df839547d31be62ca8c6
Pham Xuan Phu <michealporter803@gmail.com>
2022-03-03 16:47:05 +0800

    make it better

--- socialnetwork/settings.py
+++ socialnetwork/settings.py
@@ -155,3 +155,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configure Django App for Heroku.
+import django_on_heroku
+django_on_heroku.settings(locals())
